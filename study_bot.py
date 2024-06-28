e8f3f27e7c4f197e326105c94ffcd3b7c14d0579
BuildTools <unconfigured@null.spigotmc.org>
2024-06-29 00:03:42 +0800

    add function codes

--- study_bot.py
+++ study_bot.py
@@ -16,21 +16,39 @@ bot = commands.Bot()
     description = "Join the course channel provided in the argument."
 )
 async def join_course(interaction: discord.Interaction, name: str):
-    pass
+    for c in name[:4]:
+        if not c.isalpha():
+            await interaction.respond(f"Error: name is invalid")
+            return
+    for c in name[4:]:
+        if not c.isnumeric():
+            await interaction.respond(f"Error: name is invalid")
+            return
+    formatted_name = name[:4].lower() + "_" + name[4:]
+    await interaction.respond(f"Joined channel {formatted_name}")
 
 @bot.slash_command(
     name = "create_course",
     description = "Create a new course channel."
 )
 async def create_course(interaction: discord.Interaction, name: str):
-    pass
+    for c in name[:4]:
+        if not c.isalpha():
+            await interaction.respond(f"Error: name is invalid")
+            return
+    for c in name[4:]:
+        if not c.isnumeric():
+            await interaction.respond(f"Error: name is invalid")
+            return
+    formatted_name = name[:4].lower() + "_" + name[4:]
+    await interaction.respond(f"Created channel {formatted_name}")
 
 @bot.slash_command(
     name = "list_course",
     description = "List currently existing course channels."
 )
 async def list_course(interaction: discord.Interaction):
-    pass
+    await interaction.respond("List of channels:")
 
 @bot.event
 async def on_ready():
